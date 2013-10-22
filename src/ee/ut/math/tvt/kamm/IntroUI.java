78741dab0f13d6fc9d2ec2a9f769d9e956abb9f6
Kert Pjatkin <kertp@hotmail.com>
2013-10-22 18:20:08 +0300

    import again

--- src/ee/ut/math/tvt/kamm/IntroUI.java
+++ src/ee/ut/math/tvt/kamm/IntroUI.java
@@ -11,46 +11,59 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class IntroUI {
-	public void display() {
+	
+	JFrame raam;
+	
+	IntroUI(){
+		this.raam = new JFrame("JFrame");
+	}
+	
+	public JFrame display() throws IOException{
 		// load a properties file from class path, inside static method
 		Properties prop = new Properties();
 		Properties vers = new Properties();
-		try {
-			prop.load(new FileInputStream("application.properties"));
-			vers.load(new FileInputStream("version.properties"));
-			JFrame raam = new JFrame("JFrame");
-			raam.setLayout(new GridLayout(6, 1));
-			raam.setSize(700, 500);
-			JLabel versioon = new JLabel("Version: "
-					+ vers.getProperty("build.major.number") + "."
-					+ vers.getProperty("build.minor.number") + "."
-					+ vers.getProperty("build.revision.number"));
-			JLabel tiimi_nimi = new JLabel("Team name: "
-					+ prop.getProperty("Team.name"));
-			JLabel liider = new JLabel("Team leader: "
-					+ prop.getProperty("Team.leader"));
-			JLabel liidri_email = new JLabel("Team leader email: "
-					+ prop.getProperty("Team.leader.email"));
-			JLabel tiim = new JLabel("Team members:" + " "
-					+ prop.getProperty("Team.member.1") + ", "
-					+ prop.getProperty("Team.member.2") + ", "
-					+ prop.getProperty("Team.member.3") + ", "
-					+ prop.getProperty("Team.member.4"));
-			versioon.setFont(new Font("Serif", Font.PLAIN, 20));
-			tiim.setFont(new Font("Serif", Font.PLAIN, 20));
-			tiimi_nimi.setFont(new Font("Serif", Font.PLAIN, 20));
-			liider.setFont(new Font("Serif", Font.PLAIN, 20));
-			liidri_email.setFont(new Font("Serif", Font.PLAIN, 20));
-			raam.add(new JLabel(new ImageIcon(prop.getProperty("Team.logo"))));
-			raam.add(tiimi_nimi);
-			raam.add(liider);
-			raam.add(liidri_email);
-			raam.add(tiim);
-			raam.add(versioon);
-			raam.setVisible(true);
-		} catch (IOException e) {
-
-		}
+		prop.load(new FileInputStream("application.properties"));
+		vers.load(new FileInputStream("version.properties"));
+		raam.setLayout(new GridLayout(6, 1));
+		raam.setSize(700, 500);
+		JLabel versioon = new JLabel("Version: "
+				+ vers.getProperty("build.major.number") + "."
+				+ vers.getProperty("build.minor.number") + "."
+				+ vers.getProperty("build.revision.number"));
+		JLabel tiimi_nimi = new JLabel("Team name: "
+				+ prop.getProperty("Team.name"));
+		JLabel liider = new JLabel("Team leader: "
+				+ prop.getProperty("Team.leader"));
+		JLabel liidri_email = new JLabel("Team leader email: "
+				+ prop.getProperty("Team.leader.email"));
+		JLabel tiim = new JLabel("Team members:" + " "
+				+ prop.getProperty("Team.member.1") + ", "
+				+ prop.getProperty("Team.member.2") + ", "
+				+ prop.getProperty("Team.member.3") + ", "
+				+ prop.getProperty("Team.member.4"));
+		versioon.setFont(new Font("Serif", Font.PLAIN, 20));
+		tiim.setFont(new Font("Serif", Font.PLAIN, 20));
+		tiimi_nimi.setFont(new Font("Serif", Font.PLAIN, 20));
+		liider.setFont(new Font("Serif", Font.PLAIN, 20));
+		liidri_email.setFont(new Font("Serif", Font.PLAIN, 20));
+		raam.add(new JLabel(new ImageIcon(prop.getProperty("Team.logo"))));
+		raam.add(tiimi_nimi);
+		raam.add(liider);
+		raam.add(liidri_email);
+		raam.add(tiim);
+		raam.add(versioon);
+		raam.setVisible(true);
+		return raam;
+	}
+	
+	public void setVisible(boolean value){
+//		return;
+		this.raam.setVisible(value);
+	}
+	
+	public void setAlwaysOnTop(boolean value){
+//		this.raam.setVisible(value);
+		return;
 	}
 
 }
